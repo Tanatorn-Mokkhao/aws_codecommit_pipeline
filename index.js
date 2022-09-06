acd73176661e3c9b8a770d9140ff81ec45b79d65
Tanatorn Mokkhao <tanatorn.mokkhao@gmail.com>
2022-09-06 14:42:10 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.use('/', (req,res) => {
-  res.send('Hello world mart this is my first cicd codepipeline')
+  res.send('Hello world mart this is my first cicd codepipeline v2')
 })
 
 app.listen(3000 , () => {
